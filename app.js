27672f6744297fe716fb9f32646c0a526a1ffef2
trunghieu8668 <64822814+trunghieu8668@users.noreply.github.com>
2020-08-20 22:50:18 +0700

    Update app.js
    edit NODE_ENV

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ const port = process.env.PORT || 8000
 
 if(process.env.NODE_ENV == 'production'){
   app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 //Run
 app.listen(port, () =>{
