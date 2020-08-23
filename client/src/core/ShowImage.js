494ce995002bbad85673c9314d1a373bd68f71d3
trunghieu8668 <64822814+trunghieu8668@users.noreply.github.com>
2020-08-23 12:27:57 +0700

    Update ShowImage.js
    update Link router dom

--- client/src/core/ShowImage.js
+++ client/src/core/ShowImage.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {API} from '../config'
 
 const showImage = ({item, url, className}) => {
     return (
         <div className="product-img">
-            <a className="clearfix" href={ `/product/${item._id}` } title={ item.name }>
+            <Link className="clearfix" to={ `/product/${item._id}` } title={ item.name }>
               <img className={ className ? className : "card-img-top img-fluid mb-3" } src={ `${API}/${url}/photo/${item._id}` } alt={ item.name }/>
-            </a>
+            </Link>
         </div>
     );
 };
